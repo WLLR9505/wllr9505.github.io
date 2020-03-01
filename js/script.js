fc9b573b66127a577699b83a5298d8df6a7a27e4
Weuller Jr. Souza <weuller9505@live.com>
2020-03-01 16:37:26 -0300

    Title bar

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
-function showThis(el) {
+function showThis(target, el) {
     var viewer = document.getElementById('viewer');
+    var titleBar = document.getElementById('titleBar');
 
+    titleBar.innerText = el.children[1].innerText;
     for (let i = 0; i < viewer.children.length; i++) {
-        if (viewer.children[i].id === el) {
+        if (viewer.children[i].id === target) {
             viewer.children[i].style.display = 'contents';
         } else {
             viewer.children[i].style.display = 'none';
