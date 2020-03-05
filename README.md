da51a0cc48f94dcbf0381b56d5aa7f22d8e24cf7
Weuller Jr. Souza <weuller9505@live.com>
2020-03-05 14:14:49 -0300

    Curriculum e temas adicionados

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Página pessoal no github IO
 
 > [WLLR9505](https://wllr9505.github.io)
+
+Desktop style based on **Floramour by Pohroro**
--- js/script.js
+++ js/script.js
@@ -11,3 +11,33 @@ function showThis(target, el) {
         }
     }
 }
+
+function theme() {
+
+    //---------------3---------------7---------------12---------------18->
+    //--------neon------------BW------------lime------------cyan--------->
+
+    var hour = new Date();
+    hour = hour.getHours();
+    
+    if (hour < 3) {
+        document.write('<link href="css/neon.css" rel="stylesheet" type:"text/css">');
+        return 0;
+    }
+
+    if (hour < 7) {
+        return 0;
+    }
+
+    if (hour < 12) {
+        document.write('<link href="css/lime.css" rel="stylesheet" type:"text/css">');
+        return 0;
+    }
+
+    if (hour < 18) {
+        document.write('<link href="css/cyan.css" rel="stylesheet" type:"text/css">');
+        return 0;
+    } 
+}
+
+theme();
